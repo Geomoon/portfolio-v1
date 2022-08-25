b4779831ab6d56aec154a694e048cf363630db17
geovlunaa <lunageOvanny23@gmail.com>
2022-08-25 15:45:14 -0500

    mobile header menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,25 @@
 import Image from "next/image";
 import styles from './Header.module.css';
-import {TbDownload} from 'react-icons/tb';
+import {TbDownload, TbMenu2} from 'react-icons/tb';
 import Link from "next/link";
+import {useState} from "react";
 
 export const Header = () => {
+  
+  const [showNav, setShowNav] = useState(false);
+
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
+
   return (
     <div className={styles.header} >
-      <nav>
         <div id="logo" > 
         <a href={'/'}>
           <Image src={'/images/logo.png'} width={44} height={44} id={styles.logo_img} />
         </a>
         </div>
+      <nav className={ showNav ? styles.visible : styles.hidden }>
         <div className={styles.sections}>
           <ol>
             <li><Link href="/#about">Sobre mí</Link></li>
@@ -21,6 +29,7 @@ export const Header = () => {
           <a id={styles.preview_pdf} href="/carlos-luna.pdf" target={"_blank"}><TbDownload /> PDF</a>
         </div>
       </nav>
+      <button type="button" id={styles.menu_button} onClick={toggleNav} ><TbMenu2 /></button>
     </div>
   )
 }
